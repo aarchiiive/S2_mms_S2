c4f1a98cb83d31e62c7bf22d12ee488320044058
kim08237 <73979650+kim08237@users.noreply.github.com>
2022-12-03 06:19:19 +0900

    1

---
+++ MainPage.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    jsonTest();
+    })
+    
+    function jsonTest(){
+    document.links(user1.image);
+    document.links(user1.name);
+    document.links(user1.phone);
+    document.links(user1.addr);
+    document.links(user1.SNS);
+    document.links(user1.position);
+    document.links(user1.email);
+    }
---
+++ download.js
@@ -0,0 +1,3 @@
+<a href="javascript:void(0);" onclick="downloadImg('/Content/images/mm1.jpg')" class="main-btn__download">
+  <span class="txt-hidden">명함 다운로드</span>
+</a>
