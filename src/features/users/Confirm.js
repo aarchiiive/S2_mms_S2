b4d1da3988752464626282bea478195efa9bb8c8
aarchiiive <wychrodjxz691@gmail.com>
2022-12-04 19:05:53 +0900

    검토 페이지에서 명함 샘플 만들기... 실패

--- src/features/users/Confirm.js
+++ src/features/users/Confirm.js
@@ -15,39 +15,84 @@ const Confirm = () => {
 
   console.log(users[users.length - 1]);
   return (
-    <div className="m-auto mt-10 h-screen">
-      <h3 className="text-center text-gray-600 text-2xl mb-10">
-        명함 최종 확인 단계입니다
-      </h3>
-      <h3 className="text-center  text-gray-600 text-2xl mb-10">
-        {" "}
-        명함을 생성하려면 결제를 진행해주세요
-      </h3>
-      <div
-        className="m-auto h-auto rounded-md w-96 bg-gray-200 p-5"
-        key={users[users.length - 1].id}
-      >
-        <div>
+    <div className="h-screen m-auto w-1/2 grid gap-5 md:grid-cols-1">
+      <div className="relative">
+        <h3 className="text-center text-gray-600 text-2xl mb-10">
+          명함 최종 확인 단계입니다
+        </h3>
+        <h3 className="text-center text-gray-600 text-2xl mb-10">
+          {" "}
+          명함을 생성하려면 결제를 진행해주세요
+        </h3>
           <img
-            className="h-24 w-auto rounded-md"
-            alt="업로드 이미지"
-            src={users[users.length - 1].image}
+              className="w-96 h-auto m-auto mdevice flex justify-center rounded-md"
+              alt="명함 템플릿"
+              src={users[users.length - 1].template}
           ></img>
-          <h3 className="font-bold text-lg text- text-gray-700">
-            {users[users.length - 1].name}
-          </h3>
-          <p className="font-normal text-gray-600">
-            {users[users.length - 1].memo}
-          </p>
+          <div
+          className="flex items-center justify-center"
+          key={users[users.length - 1].id}
+          >
+            <div className = "z-10 p-5">
+              <img
+                  className="h-24 w-auto rounded-md"
+                  alt="업로드 이미지"
+                  src={users[users.length - 1].image}
+              ></img>
+              <h3 className="font-bold text-lg text- text-gray-700">
+                {users[users.length - 1].name}
+              </h3>
+              <p className="font-normal text-gray-600">
+                {users[users.length - 1].memo}
+              </p>
+            </div>
+          </div>
+        
+        <div className="mt-10 w-1/3 m-auto justify-between flex">
+          <Button onClick={() => navigate("/view-templates/" + params.id)}>이전</Button>
+          <Button onClick={() => navigate("/order/" + params.id)}>결제</Button>
         </div>
-        <div className="flex gap-4"></div>
-      </div>
-
-      <div className="mt-10 w-1/3 m-auto justify-between flex">
-        <Button onClick={() => navigate("/view-templates/" + params.id)}>이전</Button>
-        <Button onClick={() => navigate("/order/" + params.id)}>결제</Button>
       </div>
     </div>
+
+
+
+    // <div className="m-auto mt-10 h-screen">
+    //   <h3 className="text-center text-gray-600 text-2xl mb-10">
+    //     명함 최종 확인 단계입니다
+    //   </h3>
+    //   <h3 className="text-center  text-gray-600 text-2xl mb-10">
+    //     {" "}
+    //     명함을 생성하려면 결제를 진행해주세요
+    //   </h3>
+    //   {/* <div
+    //     className="m-auto h-auto rounded-md w-96 bg-gray-200 p-5"
+    //     key={users[users.length - 1].id}
+    //   > */}
+    //   <div className ="m-auto h-auto w-96 relative">
+    //     <div className = "z-10 p-5">
+    //       <img
+    //           className="absolute w-full h-full mdevice justify-between rounded-md"
+    //           alt="명함 템플릿"
+    //           src={users[users.length - 1].template}
+    //       ></img>
+    //       <div>
+    //         <img
+    //           className="h-24 w-auto rounded-md"
+    //           alt="업로드 이미지"
+    //           src={users[users.length - 1].image}
+    //         ></img>
+    //         <h3 className="font-bold text-lg text- text-gray-700">
+    //           {users[users.length - 1].name}
+    //         </h3>
+    //         <p className="font-normal text-gray-600">
+    //           {users[users.length - 1].memo}
+    //         </p>
+    //       </div>
+    //       <div className="flex gap-4"></div>
+    //     </div>
+    //   </div>
+
   );
 };
 
